27f5db1a21a774a6fff2dafac6de2f6946f0d960
DarkCompet <darkcompet@gmail.com>
2022-01-30 22:56:11 +0700

    Impl disconnection process

--- DkRealtimeNetwork.cs
+++ DkRealtimeNetwork.cs
@@ -1,11 +1,15 @@
 namespace Tool.Compet.Realtime {
 	using System;
-	using System.Net.WebSockets;
 	using System.Text;
+	using System.Net.WebSockets;
 	using System.Threading;
 	using System.Threading.Tasks;
+
 	using Cysharp.Threading.Tasks;
 	using MessagePack;
+
+	using Tool.Compet.Core;
+	using Tool.Compet.Log;
 
 	/// Socket (TCP vs UDP): https://en.wikipedia.org/wiki/Nagle%27s_algorithm
 	/// TCP test for Unity client: https://gist.github.com/danielbierwirth/0636650b005834204cb19ef5ae6ccedb
@@ -23,36 +27,41 @@ namespace Tool.Compet.Realtime {
 
 	/// [From NuGet PM] Super websocket: https://www.supersocket.net/
 	public class DkRealtimeNetwork {
-		/// Caller should provide it if authentication is required by server.
-		/// For eg,. "Bearer your_access_token"
+		/// Caller should provide it if authentication is
+		/// required by server, for eg,. "Bearer your_access_token"
 		public string? authorization;
+
+		/// Communicator between server and client
+		private ClientWebSocket socket;
+
+		/// Socket url for realtime, for eg,. wss://darkcompet.com/gaming
+		private string realtimeSocketUrl;
+
+		/// To avoid allocate new array when receive message from server.
+		private ArraySegment<byte> inBuffer;
+
+		/// Indicate caller has requested close realtime network.
+		private bool disconnectRequested;
 
 		public static bool inRoom;
 		public static bool connected;
 
-		/// Realtime handler (message sender, receiver)
-		private ClientWebSocket socket;
-		/// Socket realtime network url (for eg,. wss://darkcompet.com/gaming)
-		private string socketUrl;
-
-		/// Buffering message between server/client
-		private byte[] buffer;
-
 		private long lastSentTime;
+		long testCounter;
 
-		public DkRealtimeNetwork(string socketUrl, int bufferSize = 1 << 10) {
+		public DkRealtimeNetwork(string networkSocketUrl, int bufferSize = 1 << 10) {
 			this.socket = new();
-			this.socketUrl = socketUrl;
-			this.buffer = new byte[bufferSize];
+			this.realtimeSocketUrl = networkSocketUrl;
+			this.inBuffer = new ArraySegment<byte>(new byte[bufferSize], 0, bufferSize);
 
 			// var tcp = new System.Net.Sockets.TcpClient();
 		}
 
 		/// @param `accessToken`: For user-authentication if server required. Skip pass if does not need.
-		public async Task ConnectAsync() {
+		public async UniTask ConnectAsync() {
 			var socket = this.socket;
 
-			// [Connect to server via socket]
+			// [Connect to server]
 			// Url must be started with `wss` since server using `HTTPS`
 			// For cancellation token, also see `CancellationTokenSource, CancellationTokenSource.CancelAfter()` for detail.
 			var cancellationToken = CancellationToken.None;
@@ -60,10 +69,12 @@ namespace Tool.Compet.Realtime {
 				socket.Options.SetRequestHeader("Authorization", this.authorization);
 			}
 			await socket.ConnectAsync(
-				new System.Uri(socketUrl),
+				new Uri(realtimeSocketUrl),
 				cancellationToken
 			);
-			if (BuildConfig.DEBUG) { AppLogs.Debug(this, $"Connected to realtime server: {socketUrl}"); }
+			// Mark as connected
+			connected = true;
+			if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, $"Connected to realtime server: {realtimeSocketUrl}"); }
 
 			// [Awake server]
 			// After connected, server is waiting something from each client. why???
@@ -74,40 +85,50 @@ namespace Tool.Compet.Realtime {
 			// We have to loop interval to check/receive message from server even though server has sent it to us.
 			// See: https://devblogs.microsoft.com/xamarin/developing-real-time-communication-apps-with-websocket/
 			await Task.Factory.StartNew(async () => {
-				while (true) {
-					if (BuildConfig.DEBUG) { AppLogs.Debug(this, "Receiveing data from server..."); }
+				while (!this.disconnectRequested) {
+					if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, $"Receiveing-{++testCounter} data from server..."); }
+					// If we don't wait (use await) receive-method, this loop
+					// will be run immediately even though receive-method is not completed.
+					// => so we should await receive-method to make current thread wait reading server's event.
 					await ReceiveAsync<TestMessagePackObj>();
 				}
 			}, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
 		}
 
+		/// Run in background.
 		/// bytes: Encoding.UTF8.GetBytes(DkJsons.Obj2Json(new UserProfileResponse()))
 		public async UniTask SendAsync(object? msgPackObj = null) {
-			var webSocket = this.socket;
+			if (this.disconnectRequested) {
+				if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, "Skip sending since disconnectRequested"); }
+				return;
+			}
+
+			var socket = this.socket;
 
 			// Action `send` must be performed while socket connection is open.
 			// Otherwise we get exception.
-			if (webSocket.State == WebSocketState.Open) {
-				this.lastSentTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+			if (socket.State == WebSocketState.Open) {
+				this.lastSentTime = DkUtils.CurrentUnixTimeInMillis();
 
-				var bytes = Encoding.UTF8.GetBytes("MessagePackSerializer.Serialize(msgPackObj);");
+				var outBytes = Encoding.UTF8.GetBytes("MessagePackSerializer.Serialize(msgPackObj);");
 
-				await webSocket.SendAsync(
-					new ArraySegment<byte>(bytes),
+				await socket.SendAsync(
+					new ArraySegment<byte>(outBytes),
 					WebSocketMessageType.Text,
 					true,
 					CancellationToken.None
 				);
-				AppLogs.Debug(this, "Sent to realtime server async");
+				if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, $"Sent-{++testCounter} to realtime server async"); }
 			}
 			else {
-				AppLogs.Warning(this, "Ignored send while socket-state is NOT open, current state: " + webSocket.State);
+				DkLogs.Warning(this, $"Ignored send while socket-state is NOT open, current state: {socket.State}");
 			}
 		}
 
-		public async UniTask ReceiveAsync<T>() where T : class {
+		/// Run in background.
+		private async UniTask ReceiveAsync<T>() where T : class {
 			var socket = this.socket;
-			var buffer = this.buffer;
+			var inBuffer = this.inBuffer;
 
 			// Action `send` must be performed while socket connection is open.
 			// Otherwise we get exception.
@@ -115,43 +136,56 @@ namespace Tool.Compet.Realtime {
 				// Read server-message and fill full into the buffer.
 				// If we wanna looping to read as chunks (XXX bytes), we can check with `serverResult.EndOfMessage`
 				// to detect when reading message (line) get completed.
-				var serverResult = await socket.ReceiveAsync(
-					new ArraySegment<byte>(buffer, 0, buffer.Length),
+				var inResult = await socket.ReceiveAsync(
+					inBuffer,
 					CancellationToken.None
 				);
 
 				// Server closes the connection
-				if (serverResult.CloseStatus.HasValue) {
-					AppLogs.Debug(this, "Websocket connection has closed");
+				if (inResult.CloseStatus.HasValue) {
+					if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, "Skip read message since socket was closed"); }
 				}
 				else {
 					// dkopt: avoid re-allocate new array by impl new deserialization method
 					// To copy different array-types, consider use `Buffer.BlockCopy`.
-					var resultArr = new byte[serverResult.Count];
-					Array.Copy(buffer, resultArr, serverResult.Count);
+					var fromArr = inBuffer.Array;
+					// var toArr = new byte[inResult.Count];
+					// Array.Copy(fromArr, toArr, inResult.Count);
 					// var result = MessagePackSerializer.Deserialize<T>(resultArr);
-					var result = Encoding.UTF8.GetString(buffer, 0, serverResult.Count);
+					var result = Encoding.UTF8.GetString(fromArr, 0, inResult.Count);
 
-					AppLogs.Debug(this, $"Got result after {DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - lastSentTime} millis, result: {result}");
+					DkLogs.Info(this, $"Got result after {DkUtils.CurrentUnixTimeInMillis() - lastSentTime} millis, result: {result}");
 				}
 			}
 			else {
-				AppLogs.Warning(this, "Ignored receive while socket-state is NOT open, current state: " + socket.State);
+				DkLogs.Warning(this, "Ignored receive while socket-state is NOT open, current state: " + socket.State);
 			}
 		}
 
 		public async void OnDestroy() {
 			var socket = this.socket;
 
-			await socket.CloseAsync(
-				WebSocketCloseStatus.NormalClosure,
-				"OK",
-				CancellationToken.None
-			);
+			try {
+				if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, "Caller has requested disconnect"); }
 
-			socket.Dispose();
+				this.disconnectRequested = true;
 
-			if (BuildConfig.DEBUG) { AppLogs.Debug(this, "Closed and Disposed socket"); }
+				// [Close connection and Dispose socket]
+				await socket.CloseAsync(
+					WebSocketCloseStatus.NormalClosure,
+					"OK",
+					CancellationToken.None
+				);
+				socket.Dispose();
+
+				// Mark as disconnected
+				connected = false;
+
+				if (DkBuildConfig.DEBUG) { DkLogs.Debug(this, "Closed connection and Disposed socket"); }
+			}
+			catch (Exception e) {
+				DkLogs.Warning(this, $"Error when dispose socket, error: {e.Message}");
+			}
 		}
 	}
 
